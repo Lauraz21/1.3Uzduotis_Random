3c6f51bb29329cf81f1ee28be9e0f7f70812a794
Laura <Laura@DESKTOP-BESSB6E>
2023-09-26 21:00:56 +0300

    Add project files.

---
+++ 1.3Uzduotis_Random/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace _1._3Uzduotis_Random
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = CreateRandomPassword();
+            Console.WriteLine(password);
+        }
+
+
+        public static char ReturnRandomLetter()
+        {
+
+            Random random = new Random();
+
+            int letterNumber = random.Next(65, 91);
+
+            char letter = Convert.ToChar(letterNumber);
+
+            return letter;
+        }
+
+
+        public static string CreateRandomPassword()
+        {
+            Random random = new Random();
+            List<char> password = new List<char>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                char letter = ReturnRandomLetter();
+                password.Add(letter);
+            }
+            return new string(password.ToArray());
+
+
+        }
+    }
+
+}
